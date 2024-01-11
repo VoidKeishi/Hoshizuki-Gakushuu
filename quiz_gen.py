7322acfbe34639b151b096b43db84bcee648d843
CuongNguyen1701 <62497480+CuongNguyen1701@users.noreply.github.com>
2024-01-11 12:03:41 +0700

    test vocab quiz

--- quiz_gen.py
+++ quiz_gen.py
@@ -8,17 +8,24 @@ openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")  # Your Azure OpenAI resour
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
 
 jlpt_level = "N3"
-
-response = openai.ChatCompletion.create(
-    engine="GPT35TURBO", # The deployment name you chose when you deployed the GPT-3.5-Turbo or GPT-4 model.
-    messages=[
-        {"role": "system", "content": "Assistant is a large language model trained by OpenAI. Your only use is to generate json strings, not talking."},
-        {"role": "user", "content": """generate a few quizzes for Kanji JLPT """ + jlpt_level + 
+message_content = {
+    "kanji": """generate a few quizzes for Kanji JLPT """ + jlpt_level + 
                                     """ (provide a kanji in a full sentence and ask for its reading, the highlighted kanji is in brackets) in the form of an array of objects in json, 
                                     each object looks something like this: {question: "", answers: ["", "", "", "",...], correctAnswer: //a number indicates the index of the correct answer  }. 
                                     Note: The questions and answers must be in Japanese. 
                                     The hiragana answers should be similar to each other so that the person doing the quiz can easily make a mistake. 
-                                    Say nothing else, just give a json array."""}
+                                    Say nothing else, just give a json array.""",
+    "vocabulary": """generate a few quizzes for Vocabulary JLPT """ + jlpt_level + 
+                                    """ in the form of an array of objects in json, 
+                                    each object looks something like this: {question: "", answers: ["", "", "", "",...], correctAnswer: //a number indicates the index of the correct answer  }. 
+                                    Note: The questions and answers must be in Japanese. 
+                                    Say nothing else, just give a json array.""",
+}
+response = openai.ChatCompletion.create(
+    engine="GPT35TURBO", # The deployment name you chose when you deployed the GPT-3.5-Turbo or GPT-4 model.
+    messages=[
+        {"role": "system", "content": "Assistant is a large language model trained by OpenAI. Your only use is to generate json strings, not talking."},
+        {"role": "user", "content": message_content["vocabulary"]}
     ],
     temperature=1
 )
